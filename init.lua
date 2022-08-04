96505d09fd68fa40f2161801c0f44bab2c7aac00
francocalvo <dev@francocalvo.ar>
2022-08-04 15:32:38 -0300

    ADD: README and finish first stable version

--- init.lua
+++ init.lua
@@ -1,21 +1,68 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.indentline"
-require "user.alpha"
-require "user.whichkey"
-require "user.autocommands"
+require "user.options"                    -- OK - updated up to  5b240a9
+require "user.keymaps"                    -- OK
+require "user.plugins"                    -- OK - See below
+require "user.colorscheme"                -- OK
+require "user.cmp"                        -- OK - Pretty similar, without tabnine
+require "user.lsp"                        -- OK - updated up to 5b240a9 without rust
+require "user.telescope"                  -- OK - updated up to  5b240a9
+require "user.treesitter"                 -- OK - without textobjects part at the end
+require "user.autopairs"                  -- OK
+require "user.comment"                    -- OK
+require "user.gitsigns"                   -- OK
+require "user.nvim-tree"                  -- OK - updated up to  5b240a9
+require "user.bufferline"                 -- OK
+require "user.lualine"                    -- OK - updated up to 5b240a9
+-- require "user.toggleterm"
+require "user.project"                    -- OK
+require "user.impatient"                  -- OK
+require "user.indentline"                 -- OK - updated up to 5b240a9
+require "user.alpha"                      -- OK
+require "user.whichkey"                   -- OK
+require "user.autocommands"               -- Not updated
+require "user.illuminate"                 -- Not updated
+require "user.neoscroll"                  -- OK
+require "user.todo-comments"              -- OK
+require "user.nvim-webdev-icons"          -- OK - updated up to 5b240a9
+require "user.numb"                       -- OK
+
+require "user.vimwiki"                    -- TODO: Finish setup
+
+require "user.functions"                  -- updated up to  5b240a9
+require "user.navic"                      -- updated up to  5b240a9
+require "user.winbar"                     -- FIX: Not working?
+
+
+
+-- Not added nor checked from his repo:
+-- - autocommands
+-- - lsp-inlayhints
+-- - project
+-- - hop
+-- - matchup
+-- - dial
+-- - colorizer
+-- - spectre
+-- - zen-mode
+-- - bookmark
+-- - symbol-outline
+-- - git-blame
+-- - gitlinker
+-- - notify
+-- - ts-context
+-- - registers
+-- - sniprun
+-- - dap
+-- - lir
+-- - cybu
+-- - bfs
+-- - crates
+-- - dressing
+-- - tabout
+-- - fidget
+-- - browse
+-- - auto-session
+-- - jaq
+-- - surround
+-- - harpoon
+-- - lab
+-- - vim-slash
